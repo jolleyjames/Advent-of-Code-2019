350a3078e8b309f3c3500f77434ce9984b94a772
James Jolley <james@jolley.co>
2019-12-10 23:30:58 -0500

    Finished Day 10, part 2

--- day10/day10.py
+++ day10/day10.py
@@ -7,6 +7,8 @@ Advent of Code 2019, day 10.
 """
 
 import numpy as np
+from itertools import groupby
+import sys
 
 def read_asteroids(path):
     '''Read the file and return a list of numpy arrays of the coordinates of
@@ -20,31 +22,82 @@ def read_asteroids(path):
                 a.append(np.array((x,y)))
     return a
 
-def unique_directions(from_, to_list):
-    '''Find the number of unique directions from the position from_ to each of
-    the positions in to_list.
+def asteroids_by_direction(from_, to_list):
+    '''For the asteroid in from_, return a dictionary of lists of the asteroids
+    in to_list, indexed by the direction. The asteroids in each list are sorted
+    by increasing distinace from from_.
     Example: from_ == [0,0], to_list = [[2,0],[6,4],[15,10],[0,8]]
-    Returns [[1,0],[3,2],[0,1]]: [6,4] and [15,10] have the same direction,
-    [3,2]
+    Returns {(1,0):[(2,0)],
+             (3,2):[(6,4),(15,10)],
+             (0,1):[(0,8)]}
     '''
-    distances = [to - from_ for to in to_list]
-    directions = [d/np.gcd(*d) for d in distances]
-    return set(tuple(d) for d in directions)
+    def get_direction(from_, to):
+        distance = to - from_
+        return tuple(distance//np.gcd(*distance))
+    keylambda = lambda t: get_direction(from_, t)
+    to_list.sort(key=keylambda)
+    rval = {k:list(v) for k,v in groupby(to_list, key=keylambda)}
+    for key in rval:
+        rval[key].sort(key=lambda t: (from_[0]-t[0])**2 + (from_[1]-t[1])**2)
+        rval[key] = list(map(tuple, rval[key]))
+    return rval
 
-def part1(path):
-    '''From the specified file, find the asteroid that can directly see the
-    most other asteroids.'''
+def find_best_location(path):
+    '''From the specified file, find the best location for the asteroid
+    monitoring station. This is the asteroid that has the most other asteroids
+    directly in the line of sight, which is the same as the number of unique
+    directions to other asteroids.
+    '''
     asteroids = read_asteroids(path)
-    # Will hold tuples where [0] is the number of unique directions to other
-    # asteroids, which is the same as the number of asteroids directly in the
-    # line of sight; [1] is the coordinates of the asteroid
-    seen_by = []
+    best_asteroid = None
+    best_directions = None
     for n in range(len(asteroids)):
-        directions = unique_directions(asteroids[n], asteroids[:n]+asteroids[n+1:])
-        seen_by.append((len(directions), tuple(asteroids[n])))
-    seen_by.sort(key=lambda a:-a[0])
-    return seen_by[0]
+        directions = asteroids_by_direction(asteroids[n], asteroids[:n]+asteroids[n+1:])
+        if best_asteroid is None or len(directions) > len(best_directions):
+            best_asteroid, best_directions = tuple(asteroids[n]), directions
+    return (best_asteroid, best_directions)
+
+def order_of_destruction(directions):
+    '''From the dictionary of direction to asteroids, return a list with the
+    asteroids in order of when they will be destroyed.'''
+    # The numpy.arctan2 function returns the angle in radians given x and y
+    # coordinates, but the angles are in the range [-pi, pi]. To get them in
+    # the range [-pi/2, +3pi/2], add 2pi to angles that are less than -pi/2.
+    def get_angle(direction):
+        x, y = direction
+        angle = np.arctan2(y, x)
+        return angle if angle >= -np.pi/2 else angle + 2*np.pi
+    directions = [(get_angle(k),v) for k,v in directions.items()]
+    directions.sort()
+    directions = [d[1] for d in directions]
+    order = []
+    while directions:
+        order += [d.pop(0) for d in directions]
+        directions = [d for d in directions if len(d) > 0]
+    return order
+    
+def part1(path):
+    return partX(path, 1)
+
+def part2(path):
+    return partX(path, 2)
+    
+def partX(path, part):
+    best_asteroid, best_directions = find_best_location(path)
+    if part == 1:
+        return (len(best_directions), best_asteroid)
+    elif part == 2:
+        return order_of_destruction(best_directions)
+    else:
+        raise ValueError('part argument must be 1 or 2')
+        
 
 if __name__ == '__main__':
-    print(part1('day10/input.txt'))
+    if sys.argv[1] == '1':
+        print(part1('day10/input.txt'))
+    elif sys.argv[1] == '2':
+        a200 = part2('day10/input.txt')[199]
+        print(a200[0]*100 + a200[1])
+    else:
+        raise ValueError('part argument must be 1 or 2')
     
--- day10/test_day10.py
+++ day10/test_day10.py
@@ -17,18 +17,34 @@ class TestDay10(unittest.TestCase):
         actual = [tuple(a) for a in d.read_asteroids('day10/test1.txt')]
         self.assertCountEqual(exp, actual)
         
-    def test_unique_directions(self):
+    def test_asteroids_by_direction(self):
         from_ = np.array([3,0])
-        to_list = list(map(np.array, [[5,0],[9,4],[18,10],[3,8]]))
-        exp = [(1,0),(3,2),(0,1)]
-        self.assertCountEqual(d.unique_directions(from_, to_list), exp)
-        
+        to_list = list(map(np.array, [[18,10],[5,0],[3,8],[9,4]]))
+        exp = {(1,0):[(5,0)],
+               (3,2):[(9,4),(18,10)],
+               (0,1):[(3,8)]}
+        self.assertEqual(d.asteroids_by_direction(from_, to_list), exp)        
+
     def test_part1(self):
         self.assertEqual(d.part1('day10/test1.txt'), (8,(3,4)))
         self.assertEqual(d.part1('day10/test2.txt'), (33,(5,8)))
         self.assertEqual(d.part1('day10/test3.txt'), (35,(1,2)))
         self.assertEqual(d.part1('day10/test4.txt'), (41,(6,3)))
         self.assertEqual(d.part1('day10/test5.txt'), (210,(11,13)))
+        
+    def test_order_of_destruction(self):
+        order = d.part2('day10/test5.txt')
+        self.assertEqual(order[0], (11,12))
+        self.assertEqual(order[1], (12,1))
+        self.assertEqual(order[2], (12,2))
+        self.assertEqual(order[9], (12,8))
+        self.assertEqual(order[19], (16,0))
+        self.assertEqual(order[49], (16,9))
+        self.assertEqual(order[99], (10,16))
+        self.assertEqual(order[198], (9,6))
+        self.assertEqual(order[199], (8,2))
+        self.assertEqual(order[200], (10,9))
+        self.assertEqual(order[298], (11,1))
         
 if __name__ == '__main__':
     unittest.main()
