8b7ce70a8d1166c2ec14284c58dd3e5f151e1c8e
James Jolley <james@jolley.co>
2019-12-23 12:04:52 -0500

    Using matrix multiplication for FFT algorithm

--- day16/day16.py
+++ day16/day16.py
@@ -20,16 +20,10 @@ def get_pattern(phase, limit=None):
 def fft(signal):
     '''Process the signal using the Flawed Frequency Transmission algorithm.
     signal is a numpy array of digits. Returns another string of digits.'''
-    #TODO potentially improve performance by:
-    # 1) factoring out string processing -- DONE
-    # 2) using matrix multiplication for all signals at once
-    out = []
-    for phase in range(1, len(signal)+1):
-        pattern = np.array(list(get_pattern(phase, len(signal))))
-        d = np.sum(signal * pattern)
-        d = d % 10 if d >= 0 else -d % 10
-        out.append(d)
-    return np.array(out)
+    matrix = [np.array(list(get_pattern(phase, len(signal)))) for phase in range(1, len(signal)+1)]
+    matrix = np.array(matrix)
+    out = np.matmul(signal, np.transpose(matrix))
+    return np.vectorize(lambda d: d % 10 if d >= 0 else -d % 10)(out)
 
 def part1(path):
     with open(path, 'r') as fobj:
