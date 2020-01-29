5ebcf4ac9ef4d2a91f8bce1b859c210c244e4c32
James Jolley <james@jolley.co>
2020-01-28 20:28:45 -0500

    Added next_keys() function to Day 18

--- day18/day18.py
+++ day18/day18.py
@@ -6,6 +6,8 @@ Advent of Code 2019, day 18.
 @author: James Jolley, james@jolley.co
 """
 
+from collections import deque
+
 def read_map(path):
     '''Read the map at the specified path. Returns as a dict with (x,y)
     coordinates as a 2-tuple key, linked to a character of what exists at
@@ -28,7 +30,7 @@ def neighbor_coords(coord):
     x,y = coord
     return ((x-1,y),(x+1,y),(x,y-1),(x,y+1))
 
-def neighbors(coords):
+def get_neighbors(coords):
     '''Return a dict of each (x,y) coordinate in coords, keyed to a set of all
     of its neighbors.'''
     neighbor_dict = {}
@@ -41,3 +43,34 @@ def reverse_coord_dict(coords):
     using only the starting location (indicated by '@') and the key and door
     locations (indicated by lowercase and uppercase letters respecitvely).'''
     return {v:k for k,v in coords.items() if v != '.'}
+
+def next_keys(coord_to_loc, loc_to_coord, neighbors, from_, steps=0, keys=''):
+    '''Find the keys that are reachable from the starting coordinate, given
+    the set of keys already collected. Returns only keys that are not blocked
+    by unopened doors or other keys. Returns a dict of the key name (lowercase
+    letter) keyed to the minimum number of steps to reach it.'''
+    dist = {from_:steps}
+    keys_found = {}
+    to_search = deque([(*n,steps+1) for n in neighbors[from_]])
+    while to_search:
+        x,y,this_dist = to_search.popleft()
+        # Do not continue evaluating this path if the door is locked.
+        # Locked door is indicated by an uppercase letter whose corresponding
+        # lowercase letter is not in the pre-supplied set of keys.
+        loc = coord_to_loc[(x,y)]
+        if (ord('A') <= ord(loc) <= ord('Z')) and loc.lower() not in keys:
+            continue
+        # If this location is a key that hasn't yet been found, add it to the
+        # found keys dict, with the number of steps to this key as the value.
+        # Then stop evaluating this path.
+        if (ord('a') <= ord(loc) <= ord('z')) and \
+           loc not in keys and loc not in keys_found:
+            keys_found[loc] = this_dist
+            continue
+        # Add this location to the dict of minimum distances
+        dist[(x,y)] = this_dist
+        # Add the un-evaluated neighbors to the queue
+        to_search.extend([(x_,y_,this_dist+1) for x_,y_ in neighbors[(x,y)] if (x_,y_) not in dist])
+    # Return the keys found and the minimum distances to each
+    return keys_found
+    
--- day18/test_day18.py
+++ day18/test_day18.py
@@ -17,7 +17,7 @@ class TestDay18(unittest.TestCase):
     def test_neighbor_coords(self):
         self.assertCountEqual(d.neighbor_coords((-4,8)), [(-4,7),(-4,9),(-3,8),(-5,8)])
         
-    def test_neighbors(self):
+    def test_get_neighbors(self):
         check = {}
         check[(1,1)] = set([(2,1)])
         check[(2,1)] = set([(1,1),(3,1)])
@@ -26,11 +26,27 @@ class TestDay18(unittest.TestCase):
         check[(5,1)] = set([(4,1),(6,1)])
         check[(6,1)] = set([(5,1),(7,1)])
         check[(7,1)] = set([(6,1)])
-        self.assertEqual(d.neighbors(d.read_map('day18/test1.txt')), check)
+        self.assertEqual(d.get_neighbors(d.read_map('day18/test1.txt')), check)
         
     def test_reverse_coord_dict(self):
         check = {'b':(1,1), 'A':(3,1), '@':(5,1), 'a':(7,1)}
         self.assertEqual(d.reverse_coord_dict(d.read_map('day18/test1.txt')), check)
+        
+    def test_next_keys(self):
+        coord_to_loc = d.read_map('day18/test1.txt')
+        loc_to_coord = d.reverse_coord_dict(coord_to_loc)
+        neighbors = d.get_neighbors(coord_to_loc)
+        self.assertEqual(d.next_keys(coord_to_loc,loc_to_coord,neighbors,loc_to_coord['@']),{'a':2})
+        self.assertEqual(d.next_keys(coord_to_loc,loc_to_coord,neighbors,loc_to_coord['a'],2,'a'),{'b':8})
+        self.assertEqual(d.next_keys(coord_to_loc,loc_to_coord,neighbors,(2,1)),{'b':1})
+        self.assertEqual(d.next_keys(coord_to_loc,loc_to_coord,neighbors,loc_to_coord['b'],1,'b'),{})
+        
+        coord_to_loc = d.read_map('day18/test2.txt')
+        loc_to_coord = d.reverse_coord_dict(coord_to_loc)
+        neighbors = d.get_neighbors(coord_to_loc)
+        self.assertEqual(d.next_keys(coord_to_loc,loc_to_coord,neighbors,loc_to_coord['c'],1000,'abc'),{'e':1014, 'd':1024})
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
